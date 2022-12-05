ea2cac3efe4f14f41c4b09afe784a05bf8643b18
Dylan Owens <90014103+DylanOwens-Dev@users.noreply.github.com>
2022-12-05 11:58:21 -0500

    added escape key event and cleaned up script

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
 'use strict';
 
+//variables of object needs for script from html
 let modalBtns = document.querySelectorAll('.show-modal');
 let modal = document.querySelector('.modal');
 let body = document.querySelector('body');
 let overlay = document.querySelector('.overlay');
 let closeBtn = document.querySelector('.close-modal');
 
+//function that hides modal
 function hideModal() {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 }
 
-for (let i = 0; i < modalBtns.length; i++) {
-  modalBtns[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+//function that show modal
+function showModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 }
 
-closeBtn.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+//loop through the btns and show modal on click
+for (let i = 0; i < modalBtns.length; i++)
+  modalBtns[i].addEventListener('click', showModal);
+
+//hide modal on click of X btn
+closeBtn.addEventListener('click', hideModal);
+
+//hide modal on click of overlay
+overlay.addEventListener('click', hideModal);
+
+//hide modal on click of escape key
+window.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    hideModal();
+  }
 });
-
-overlay.addEventListener('click', hideModal());
-
-// if (overlay.classList.contains('hidden')) {
-// } else {
-//   overlay.addEventListener('click', function () {
-//     modal.classList.add('hidden');
-//     overlay.classList.add('hidden');
-//   });
-// }
